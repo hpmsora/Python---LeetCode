286e1f3980d1c3fd129d2efc046e4a22608e3512
Won Yong Ha <hpmsora@gmail.com>
2024-01-10 03:41:24 -0500

    64. Minimum Path Sum optimized
    64. Minimum Path Sum optimized

--- Solutions/0064_Minimum_Path_Sum.py
+++ Solutions/0064_Minimum_Path_Sum.py
@@ -2,45 +2,18 @@ class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         m = len(grid) # n row
         n = len(grid[0]) # n column
-        dp = [[ -1 for _ in range(n)] for _ in range(m)]
 
-        queue = [[0, 0]]
-        queue_set = set()
-        queue_set.add((0, 0))
+        # first row update
+        for y in range(1, n):
+            grid[0][y] += grid[0][y-1]
+        
+        # first column update
+        for x in range(1, m):
+            grid[x][0] += grid[x-1][0]
 
-        val = 0
-        while queue:
-            x, y = queue.pop(0)
+        # rest of cells
+        for x in range(1, m):
+            for y in range(1, n):
+                grid[x][y] += min([grid[x-1][y], grid[x][y-1]])
 
-            # Update start --------------------------
-            # Up side
-            up = float('inf')
-            if not x == 0:
-                up = dp[x - 1][y]
-
-            # Left side
-            left = float('inf')
-            if not y == 0:
-                left = dp[x][y-1]
-            elif x == 0: # First cell
-                up = 0
-                left = 0
-            
-            val = grid[x][y] + min([up, left])
-            dp[x][y] = val 
-            # Update end --------------------------
-
-            # Queue add start --------------------------
-            # Add down
-            if x + 1 < m:
-                if not (x+1, y) in queue_set:
-                    queue.append((x+1, y))
-                    queue_set.add((x+1, y))
-            # Add right
-            if y + 1 < n:
-                if not (x, y + 1) in queue_set:
-                    queue.append((x, y + 1))
-                    queue_set.add((x, y + 1))
-            # Queue add end --------------------------
-
-        return val
+        return grid[m-1][n-1]
