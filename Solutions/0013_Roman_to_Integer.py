5dee728f5825cfa198f580b51330ca19c5950156
Won Yong Ha <hpmsora@gmail.com>
2024-02-09 02:08:08 -0500

    Update 0013_Roman_to_Integer.py

--- Solutions/0013_Roman_to_Integer.py
+++ Solutions/0013_Roman_to_Integer.py
@@ -1,23 +1,29 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman_dict = {
-            "I" : 1,
-            "V" : 5,
-            "X" : 10,
-            "L" : 50,
-            "C" : 100,
-            "D" : 500,
-            "M" : 1000
+        # r_num_dict
+        r_dict = {
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
         }
-
-        total = 0
-        pre_digit = roman_dict[s[0]]
-        for each_digit in s:
-            value = roman_dict[each_digit]
-            if value <= pre_digit:
-                total += value
-            else:
-                total += value
-                total -= 2*pre_digit
-            pre_digit = value
-        return total
+        
+        # variables:
+        sol = 0
+        prev = float('inf')
+        
+        # Loop - each s
+        for each_s in s:
+            num = r_dict[each_s]
+            if num > prev: # speical case
+                sol += num - prev*2
+            else: # normal case
+                sol += num
+            
+            # update prev
+            prev = num
+        
+        return sol
