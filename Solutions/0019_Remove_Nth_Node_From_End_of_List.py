f2d6ed7f256ca7faf6539a0ca3c51cab96b67d7f
Won Yong Ha <hpmsora@gmail.com>
2024-03-03 00:33:00 -0500

    19. Remove Nth Node From End of List
    19. Remove Nth Node From End of List optimized

--- Solutions/0019_Remove_Nth_Node_From_End_of_List.py
+++ Solutions/0019_Remove_Nth_Node_From_End_of_List.py
@@ -5,29 +5,38 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-
+        # Save the head
         dummy = ListNode(val=-1, next=head)
-        order_dict = {}
 
-        index = 1
+        # Save the node to dictionary
+        # key: index (int), val: node (ListNode)
+        node_dict = {}
 
-        prev = head
+        # index save
+        index = 0
+
+        # Loop - all node
         while head:
-            order_dict[index] = head
+            # Update node_dict
+            node_dict[index] = head
 
-            index += 1
-            prev = head
+            # Update head
             head = head.next
-        index -= 1
 
-        head = dummy.next
-        if index-n == 0:
-            return head.next
-        head = order_dict[index - n]
+            # Update index
+            index += 1
 
-        if head.next.next:
-            head.next = head.next.next
-        else:
-            head.next = None
+        # Find total length
+        index = index - 1
 
+        # Removing index
+        removing_index = index - n
+
+        # Removing condition
+        if removing_index == -1: # removing first index
+            dummy.next = dummy.next.next
+        else: # removing not first index
+            node_dict[removing_index].next = node_dict[removing_index].next.next
+
+        # RETURN
         return dummy.next
