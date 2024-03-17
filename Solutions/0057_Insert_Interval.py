5d88e00d9179b1bb59fdeb8f1016fa7dad406771
Won Yong Ha <hpmsora@gmail.com>
2024-03-17 13:38:25 +0900

    Update 0057_Insert_Interval.py
    Daily update

--- Solutions/0057_Insert_Interval.py
+++ Solutions/0057_Insert_Interval.py
@@ -1,35 +1,47 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        n_start, n_end = newInterval
-        sol_index = []
-        new_start = n_start
-        new_end = n_end
-        cut_index = 0
-        isLast = True
-        for index in range(len(intervals)):
-            start, end = intervals[index]
-            if start > n_end:
-                cut_index = index
-                isLast = False
+        if len(intervals) == 0:
+            return [newInterval]
+        new_start, new_end = newInterval
+
+        index = 0
+        start_index = float("inf")
+        end_index = float("-inf")
+        new_start_int = new_start
+        new_end_int = new_end
+
+        for start, end in intervals:
+            isValid = False
+            if start <= new_start and new_start <= end: # Start point check
+                isValid = True
+            elif start <= new_end and new_end <= end:
+                isValid = True
+            elif new_start <= start and start <= new_end:
+                isValid = True
+            elif new_start <= end and end <= new_end:
+                isValid = True
+            elif new_end < start:
                 break
-            elif end < n_start:
-                continue
-            sol_index.append(index)
-            new_start = min([new_start, start])
-            new_end = max([new_end, end])
-        if isLast:
-            cut_index = len(intervals) - 1
-        if sol_index:
-            intervals[sol_index.pop(0)] = [new_start, new_end]
-            if sol_index:
-                pop_index = sol_index[0]
-                for _ in sol_index:
-                    intervals.pop(pop_index)
+
+            if isValid:
+                start_index = min(start_index, index)
+                end_index = max(end_index, index)
+
+                new_start_int = min(new_start_int, start)
+                new_end_int = max(new_end_int, end)
+                print(start_index)
+
+            index += 1
+
+        newInterval_final = [new_start_int, new_end_int]
+
+        if index == 0 and start_index == float("inf"):
+            sol = [[new_start_int, new_end_int]] + intervals
+        elif index == len(intervals) and start_index == float("inf"):
+            sol = intervals + [[new_start_int, new_end_int]]
+        elif start_index == float("inf"):
+            sol = intervals[:index] + [[new_start_int, new_end_int]] + intervals[index:]
         else:
-            if isLast:
-                intervals.append(newInterval)
-            elif cut_index == 0:
-                intervals = [newInterval] + intervals
-            else:
-                intervals = intervals[:cut_index] + [newInterval] + intervals[cut_index:]
-        return intervals
+            sol = intervals[:start_index] + [[new_start_int, new_end_int]] + intervals[end_index + 1:]
+
+        return sol
