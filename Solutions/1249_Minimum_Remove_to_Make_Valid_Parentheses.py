c9711cbc9c14143bd3c3ee950c815bfc046ae696
Won Yong Ha <hpmsora@gmail.com>
2024-04-06 07:48:04 -0400

    Update 1249_Minimum_Remove_to_Make_Valid_Parentheses.py

--- Solutions/1249_Minimum_Remove_to_Make_Valid_Parentheses.py
+++ Solutions/1249_Minimum_Remove_to_Make_Valid_Parentheses.py
@@ -1,24 +1,39 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
+        # solution list
         par_list = []
 
+        # removed set
         remove_list = set()
+
+        # loop - all letter s
         for index in range(len(s)):
+            # get left side letter
             each_s = s[index]
-            if each_s == "(":
+
+            if each_s == "(": # check open
                 par_list.append(("(", index))
-            elif each_s == ")":
+            elif each_s == ")": # check close
                 if not par_list:
                     remove_list.add(index)
                     continue
                 
+                # remove
                 par_list.pop()
+        
+        # loop - remove the letter
         for _, index in par_list:
             remove_list.add(index)
 
+        # solution string
         sol = ""
+
+        # loop - all letter s
         for index in range(len(s)):
+            # get each letter
             each_s = s[index]
             if not index in remove_list:
                 sol += each_s
+        
+        # RETURN - solution strig
         return sol
