3c2aac3993af37c36f64482e150a6a527846dbd4
Won Yong Ha <hpmsora@gmail.com>
2024-10-29 16:51:58 -0400

    Update 0125_Valid_Palindrome.py

--- Solutions/0125_Valid_Palindrome.py
+++ Solutions/0125_Valid_Palindrome.py
@@ -1,26 +1,24 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         char_list = set(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0','1','2','3','4','5','6','7','8','9'])
-        len_s = len(s)
-        count = len_s - 1
-        for index in range(len_s):
-            each_s = s[index].lower()
-            if each_s in char_list:
-                while True:
-                    each_s_rev = s[count].lower()
-                    print(each_s_rev)
-                    if index == count:
-                        return True
-                    if each_s_rev in char_list:
-                        if each_s_rev == each_s:
-                            count -= 1
-                            break
-                        else:
-                            return False
-                    else:
-                        count -= 1
-                if index == count:
-                    return True
-            else:
-                if index == count:
-                    return True
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            # Next Left
+            while left < right and not s[left].lower() in char_list:
+                left += 1
+            left_letter = s[left].lower()
+            left += 1
+
+            # Next Right
+            while 0 <= right and not s[right].lower() in char_list:
+                right -= 1
+            right_letter = s[right].lower()
+            right -= 1
+
+            if not left_letter in char_list:
+                return True
+
+            if not (left_letter == right_letter):
+                return False
+        return True
