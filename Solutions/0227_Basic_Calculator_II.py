cfe02c5b2d81b554497b2d896f297cc478c56ad6
Won Yong Ha <hpmsora@gmail.com>
2024-11-05 14:49:02 -0500

    Update 0227_Basic_Calculator_II.py

--- Solutions/0227_Basic_Calculator_II.py
+++ Solutions/0227_Basic_Calculator_II.py
@@ -1,43 +1,56 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        add_sub_list = []
+        # Declare constants
+        mul_div_set = {"*", "/"}
+        add_sub_set = {"+", "-"}
 
-        str_num = ""
-        add_sub_set, mul_div_set = {"+", "-"}, {"*", "/"}
+        # Declare new list removed mul/div
+        new_s_list = []
+
+        # Declare new num string
+        num = ""
+
+        # 1. Calculate mul/div
+        # Loop - string s to each letter
         for each_s in s + "=":
+            # Skip space
             if each_s == " ":
                 continue
             
-            if each_s in add_sub_set or each_s in mul_div_set:
-                if add_sub_list and add_sub_list[-1] in mul_div_set:
-                    last_sign = add_sub_list.pop()
-                    if last_sign == "*":
-                        add_sub_list[-1] = int(add_sub_list[-1] * int(str_num))
+            # Split and calculation
+            if each_s in mul_div_set or each_s in add_sub_set or each_s == "=": # Operation
+                # String(num) -> int(num)
+                num = int(num)
+                if new_s_list and (new_s_list[-1] in mul_div_set or each_s == "="):
+                    if new_s_list[-1] == "*":
+                        new_s_list[-2] *= num
+                    elif new_s_list[-1] == "/":
+                        new_s_list[-2] = int(new_s_list[-2] / num)
                     else:
-                        add_sub_list[-1] = int(add_sub_list[-1] / int(str_num))
-                    add_sub_list.append(each_s)
+                        new_s_list += [num, each_s]
+                    new_s_list[-1] = each_s
                 else:
-                    add_sub_list += [int(str_num), each_s]
-                str_num = ""
-            elif each_s == "=":
-                if add_sub_list and add_sub_list[-1] in mul_div_set:
-                    last_sign = add_sub_list.pop()
-                    if last_sign == "*":
-                        add_sub_list[-1] = int(add_sub_list[-1] * int(str_num))
-                    else:
-                        add_sub_list[-1] = int(add_sub_list[-1] / int(str_num))
-                else:
-                    add_sub_list += [int(str_num)]
-            else:
-                str_num += each_s
-        sol = add_sub_list[0]
+                    new_s_list += [num, each_s]
+                num = ""
+            else: # Number
+                num += each_s
+
+        # 2. Calculate add/sub
+        # Declare solution variable
+        sol = new_s_list[0]
+
+        # Loop - each mul/div removed list
         index = 1
-        while index < len(add_sub_list) - 1:
-            if add_sub_list[index] == "+":
-                index += 1
-                sol += add_sub_list[index]
-            elif add_sub_list[index] == "-":
-                index += 1
-                sol -= add_sub_list[index]
-            index += 1
+        while index < len(new_s_list)-2:
+            # Get the indexed number
+            each_new_s_list = new_s_list[index]
+
+            if each_new_s_list == "+": # Addition
+                sol += new_s_list[index+1]
+            else: # Substraction
+                sol -= new_s_list[index+1]
+
+            index += 2
+        
+        # RETURN - sol
         return sol
