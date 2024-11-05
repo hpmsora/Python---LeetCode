a53600834ed623c2ee61bcb0b18327a33813973c
Won Yong Ha <hpmsora@gmail.com>
2024-11-05 12:17:12 -0500

    Update 1249_Minimum_Remove_to_Make_Valid_Parentheses.py

--- Solutions/1249_Minimum_Remove_to_Make_Valid_Parentheses.py
+++ Solutions/1249_Minimum_Remove_to_Make_Valid_Parentheses.py
@@ -1,21 +1,20 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        par_list = []
+        sol = []
+
+        open_par = []
 
         for index, each_s in enumerate(s):
-            if each_s == ")":
-                if par_list:
-                    last_par, _ = par_list[-1]
-                    if last_par == "(":
-                        par_list.pop()
-                    else:
-                        par_list.append((")", index))
+            if each_s == "(":
+                open_par.append(index)
+            elif each_s == ")":
+                if not open_par:
+                    sol.append(index)
                 else:
-                    par_list.append((")", index))
+                    open_par.pop()
 
-            elif each_s == "(":
-                par_list.append(("(", index))
         s_list = list(s)
-        for _, index in par_list:
-            s_list[index] = ""
+        for each_index in sol + open_par:
+            s_list[each_index] = ""
+
         return ''.join(s_list)
