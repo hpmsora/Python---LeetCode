c3f7039027dfe91a8ed9a29405cb1ac3f50fb86c
Won Yong Ha <hpmsora@gmail.com>
2024-11-04 19:55:13 -0500

    Update 2914_Minimum_Number_of_Changes_to_Make_Binary_String_Beautiful.py

--- Solutions/2914_Minimum_Number_of_Changes_to_Make_Binary_String_Beautiful.py
+++ Solutions/2914_Minimum_Number_of_Changes_to_Make_Binary_String_Beautiful.py
@@ -1,29 +1,34 @@
 class Solution:
     def minChanges(self, s: str) -> int:
-        num_sequece_list = []
-
         prev_s = s[0]
         count = 1
+        sol = 0
+        isSwap = False
         for each_s in s[1:] + "=":
             if each_s == prev_s:
                 count += 1
             elif each_s == "=":
-                num_sequece_list.append(count)
-            else:
-                num_sequece_list.append(count)
-                prev_s = each_s
-                count = 1
-        
-        sol = 0
-        isSwap = False
-        for each_sequence in num_sequece_list:
-            if each_sequence % 2 == 1:
-                if not isSwap:
-                    isSwap = True
+                if count % 2 == 1:
+                    if not isSwap:
+                        isSwap = True
+                    else:
+                        sol += 1
+                        isSwap = False
                 else:
-                    sol += 1
-                    isSwap = False
+                    if isSwap:
+                        sol += 1
             else:
-                if isSwap:
-                    sol += 1 
+                prev_s = each_s
+
+                if count % 2 == 1:
+                    if not isSwap:
+                        isSwap = True
+                    else:
+                        sol += 1
+                        isSwap = False
+                else:
+                    if isSwap:
+                        sol += 1
+
+                count = 1
         return sol
