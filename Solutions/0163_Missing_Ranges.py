2edb66a2318a4e354d2efb49c779a4eaac1840c0
Won Yong Ha <hpmsora@gmail.com>
2024-11-05 15:12:12 -0500

    Update 0163_Missing_Ranges.py

--- Solutions/0163_Missing_Ranges.py
+++ Solutions/0163_Missing_Ranges.py
@@ -2,22 +2,13 @@ class Solution:
     def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
         sol = []
 
-        # Empty Case
-        if not nums:
-            return [[lower, upper]]
-
-        # Loop
-        for num in nums:
-            if lower == num: # Case 1: lower bound == current num
-                lower = num + 1
-            elif lower < num and num <= upper: # Case 2: lower < current num < upper
-                sol.append([lower, num - 1])
-                lower = num + 1
-            else: # out of upper bound
-                break
-
-        # Last bound check
-        if lower <= upper:
-            sol.append([lower, upper])
-        
+        left = lower
+        for each_nums in nums:
+            if left <= each_nums - 1:
+                sol.append([left, each_nums-1])
+                left = each_nums + 1
+            else:
+                left = each_nums + 1
+        if left <= upper:
+            sol.append([left, upper])
         return sol
