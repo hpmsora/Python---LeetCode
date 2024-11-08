105a2f0e73384ab8a91422ba0241fab99347b3cb
Won Yong Ha <hpmsora@gmail.com>
2024-11-08 13:48:37 -0500

    Update 0266_Palindrome_Permutation.py

--- Solutions/0266_Palindrome_Permutation.py
+++ Solutions/0266_Palindrome_Permutation.py
@@ -1,16 +1,16 @@
 class Solution:
     def canPermutePalindrome(self, s: str) -> bool:
-        s_dict = {}
+        letter_count = {}
 
         for each_s in s:
-            if each_s in s_dict:
-                s_dict[each_s] += 1
+            if each_s in letter_count:
+                letter_count[each_s] += 1
             else:
-                s_dict[each_s] = 1
-
+                letter_count[each_s] = 1
+        
         odd_count = 0
-        for _, val in s_dict.items():
-            if val % 2 == 1:
+        for value in letter_count.values():
+            if value % 2 == 1:
                 odd_count += 1
                 if odd_count > 1:
                     return False
