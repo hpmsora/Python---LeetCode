92686181bf56768a279ffcacff8c3ec10b700947
Won Yong Ha <hpmsora@gmail.com>
2024-11-12 15:09:31 -0500

    Update 0314_Binary_Tree_Vertical_Order_Traversal.py

--- Solutions/0314_Binary_Tree_Vertical_Order_Traversal.py
+++ Solutions/0314_Binary_Tree_Vertical_Order_Traversal.py
@@ -6,53 +6,29 @@
 #         self.right = right
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        # Special case - empty root
+        # Special case
         if not root:
             return []
-        
-        # min, max column number declare
-        min_col = 0
-        max_col = 0
-        
-        # column dictinary
-        # key: int(col)
-        # value: list(int(val))
-        col_dict = {}
-
         # BFS
-        # dp = [(TreeNode, int(col)),...]
         dp = [(root, 0)]
-        while dp:
-            # Get first node info
-            curr_node, col = dp.pop(0)
-
-            # column dictionary update
-            if col in col_dict:
-                col_dict[col].append(curr_node.val)
-            else:
-                col_dict[col] = [curr_node.val]
-
-            # min, max col update
-            if min_col > col:
-                min_col = col
-            elif max_col < col:
-                max_col = col
-
+        
+        index = 0
+        min_depth = 0
+        max_depth = 0
+        while index < len(dp):
+            curr_node, depth = dp[index]
             # Left
             if curr_node.left:
-                dp.append((curr_node.left, col - 1))
-            
+                dp.append((curr_node.left, depth-1))
+                min_depth = min(min_depth, depth-1)
             # Right
             if curr_node.right:
-                dp.append((curr_node.right, col + 1))
-
-        # solution variable
-        sol = []
-
-        # Loop - column number from min col to max col
-        for col_num in range(min_col, max_col + 1):
-            # Add to sol list the value list
-            sol.append(col_dict[col_num])
+                dp.append((curr_node.right, depth+1))
+                max_depth = max(max_depth, depth+1)
+            index += 1
+        sol = [[] for _ in range(min_depth, max_depth+1)]
         
-        # RETURN - sol
+        for each_dp, depth in dp:
+            depth = depth - min_depth
+            sol[depth].append(each_dp.val)
         return sol
