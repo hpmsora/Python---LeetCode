c58dd3928cfb0957231e97a38c2cd4fe8ae4371a
Won Yong Ha <hpmsora@gmail.com>
2024-11-14 13:27:59 -0500

    Update 0865_Smallest_Subtree_withall_the_Deepest_Nodes.py

--- Solutions/0865_Smallest_Subtree_withall_the_Deepest_Nodes.py
+++ Solutions/0865_Smallest_Subtree_withall_the_Deepest_Nodes.py
@@ -5,29 +5,47 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
-        def helper(_root, _depth):
-            if not(_root.left or _root.right):
-                return (_root, _depth)
+    def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # BFS
+        # dp List = [(node, parent's list, depth)...]
+        dp = [(root, [], 0)]
+        node_dict = {}
+        
+        same_depth = []
+        prev_depth = -1
+        while dp:
+            dp_element = dp.pop(0)
+            curr_node, parent_node_list, depth = dp_element
+            node_dict[curr_node.val] = curr_node
             
-            left_node = None
-            left_depth = -1
-            if _root.left:
-                left_node, left_depth = helper(_root.left, _depth + 1)
-            
-            right_node = None
-            right_depth = -1
-            if _root.right:
-                right_node, right_depth = helper(_root.right, _depth + 1)
-
-            if left_depth > right_depth:
-                return (left_node, left_depth)
-            elif left_depth < right_depth:
-                return (right_node, right_depth)
+            if not depth == prev_depth:
+                same_depth = [dp_element]
+                prev_depth = depth
             else:
-                return (_root, left_depth)
-
-        sol, _ = helper(root, 0)
-
-        return sol
+                same_depth.append(dp_element)
             
+            # Left
+            if curr_node.left:
+                dp.append((curr_node.left, [curr_node.val] + parent_node_list, depth + 1))
+            # Right
+            if curr_node.right:
+                dp.append((curr_node.right, [curr_node.val] + parent_node_list, depth + 1))
+        parent_list_set = []
+        heap = []
+        heapq.heapify(heap)
+        
+        for each_curr_node, each_parent_list, _ in same_depth:
+            parent_list = [each_curr_node.val] + each_parent_list
+            parent_list_set.append(set(parent_list))
+            heapq.heappush(heap, (len(parent_list), parent_list))
+            
+        _, shortest = heapq.heappop(heap)
+        for each_shortest in shortest:
+            isSol = True
+            for each_parent_list_set in parent_list_set:
+                if not each_shortest in each_parent_list_set:
+                    isSol = False
+                    break
+            if isSol:
+                return node_dict[each_shortest]
+        return []
