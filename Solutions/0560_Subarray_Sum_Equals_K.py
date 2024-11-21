a1585b37caa52285495173a1eae5b8cb232ce86d
Won Yong Ha <hpmsora@gmail.com>
2024-11-21 14:58:57 -0500

    Update 0560_Subarray_Sum_Equals_K.py

--- Solutions/0560_Subarray_Sum_Equals_K.py
+++ Solutions/0560_Subarray_Sum_Equals_K.py
@@ -1,22 +1,17 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        right = 0
-        sum_index_dict = {0:1}
-        curr_sum = 0
+        t_sum = 0
+        t_sum_dict = {}
         
         sol = 0
-        
-        while right < len(nums):
-            curr_sum += nums[right]
-            
-            diff = curr_sum - k
-            
-            if diff in sum_index_dict:
-                sol += sum_index_dict[diff]
-            
-            if curr_sum in sum_index_dict:
-                sum_index_dict[curr_sum] += 1
+        for each_nums in nums:
+            if t_sum in t_sum_dict:
+                t_sum_dict[t_sum] += 1
             else:
-                sum_index_dict[curr_sum] = 1
-            right += 1
+                t_sum_dict[t_sum] = 1
+                
+            t_sum += each_nums
+            
+            if t_sum - k in t_sum_dict:
+                sol += t_sum_dict[t_sum - k]
         return sol
