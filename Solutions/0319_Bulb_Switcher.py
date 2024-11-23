012fa8b361836530e6b63e2a9485e4ff77405199
Won Yong Ha <hpmsora@gmail.com>
2024-11-23 01:40:27 -0500

    Update 0319_Bulb_Switcher.py

--- Solutions/0319_Bulb_Switcher.py
+++ Solutions/0319_Bulb_Switcher.py
@@ -1,10 +1,13 @@
 class Solution:
     def bulbSwitch(self, n: int) -> int:
+        if n == 0:
+            return 0
+        
+        index = 1
         sol = 0
         
-        count = 1
-        
-        while count*count <= n:
+        while index**2 <= n:
             sol += 1
-            count += 1
+            index += 1
+            
         return sol
