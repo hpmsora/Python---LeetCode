568fe3e128755c0f14b6ff0daca130cec4f7a813
Won Yong Ha <hpmsora@gmail.com>
2024-11-25 14:01:39 -0500

    Update 0567_Permutation_in_String.py

--- Solutions/0567_Permutation_in_String.py
+++ Solutions/0567_Permutation_in_String.py
@@ -1,47 +1,50 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1_dict = {}
+        if len(s1) > len(s2):
+            return False 
+        s1_list_letter_freq = {}
         
         for each_s1 in s1:
-            if each_s1 in s1_dict:
-                s1_dict[each_s1] += 1
+            if each_s1 in s1_list_letter_freq:
+                s1_list_letter_freq[each_s1] += 1
             else:
-                s1_dict[each_s1] = 1
+                s1_list_letter_freq[each_s1] = 1
         
-        if len(s1) > len(s2):
-            return False
-        
-        left = 0
-        right = 0
-        len_s1 = len(s1)
+        # init
+        right = len(s1)
         curr_dict = {}
-        
-        while right < len(s2):
-            curr_letter = s2[right]
-            if curr_letter in curr_dict:
-                curr_dict[curr_letter] += 1
+        for index in range(right):
+            if s2[index] in curr_dict:
+                curr_dict[s2[index]] += 1
             else:
-                curr_dict[curr_letter] = 1
+                curr_dict[s2[index]] = 1
                 
-            if curr_letter in s1_dict:
-                while right-left + 1 > len_s1:
-                    left_letter = s2[left]
-                    
-                    if curr_dict[left_letter] == 1:
-                        del curr_dict[left_letter]
-                    else:
-                        curr_dict[left_letter] -= 1
-                    
-                    left += 1
-                if right-left + 1 == len_s1:
-                    isSol = True
-                    for letter, freq in curr_dict.items():
-                        if not s1_dict[letter] == freq:
-                            isSol = False
-                    if isSol:
-                        return True
+        def check_per(_curr_dict, _target):
+            for letter, freq in _target.items():
+                if not letter in _curr_dict:
+                    return False
+                elif not freq == _curr_dict[letter]:
+                    return False
+            return True
+        
+        # init Check
+        if check_per(curr_dict, s1_list_letter_freq):
+            return True
+        
+        # Rest
+        for index in range(right, len(s2)):
+            right_letter = s2[index]
+            if right_letter in curr_dict:
+                curr_dict[right_letter] += 1
             else:
-                left = right + 1
-                curr_dict = {}
-            right += 1
+                curr_dict[right_letter] = 1
+                
+            earliest_letter = s2[index-len(s1)]
+
+            if curr_dict[earliest_letter] == 1:
+                del curr_dict[earliest_letter]
+            else:
+                curr_dict[earliest_letter] -= 1
+            if check_per(curr_dict, s1_list_letter_freq):
+                return True
         return False
