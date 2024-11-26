c5f1fe3adfe4ce7ee5105bcd4ed1ef5ef1e27e8c
Won Yong Ha <hpmsora@gmail.com>
2024-11-26 12:45:59 -0500

    Daily

--- Solutions/0088_Merge_Sorted_Array.py
+++ Solutions/0088_Merge_Sorted_Array.py
@@ -3,6 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        for i in range(n):
-            nums1[m+i] = nums2[i]
-        nums1.sort()
+
+        index_1 = m - 1
+        index_2 = n - 1
+
+        while index_1 >= 0 or index_2 >= 0:
+
+            if index_1 < 0:
+                nums1[index_2] = nums2[index_2]
+                index_2 -= 1
+            elif index_2 < 0:
+                nums1[index_1] = nums1[index_1]
+                index_1 -= 1
+
+            elif nums1[index_1] > nums2[index_2]:
+                nums1[index_1 + index_2+1] = nums1[index_1]
+                index_1 -= 1
+            else:
+                nums1[index_1 + index_2+1] = nums2[index_2]
+                index_2 -= 1
--- Solutions/0125_Valid_Palindrome.py
+++ Solutions/0125_Valid_Palindrome.py
@@ -1,33 +1,13 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        left = 0
-        right = len(s) - 1
-        alphabet_set = {
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-        }
-        alphabet_upper_set = {
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-        }
-        numerical_set = {
-            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
-        }
-        while left < right:
-            # Left Stratch
-            while left < right and not (s[left] in alphabet_set or s[left] in alphabet_upper_set or s[left] in numerical_set):
-                left += 1
-            
-            if left == right:
-                return True
-            
-            # Right Stratch
-            while left < right and not (s[right] in alphabet_set or s[right] in alphabet_upper_set or s[right] in numerical_set):
-                right -= 1
-            if left == right:
-                return True
-            
-            if not s[left].lower() == s[right].lower():
-                return False
+        s_list = list(s)
+
+        index = 0
+        while index < len(s_list):
+            if s_list[index].isalnum():
+                s_list[index] = s_list[index].lower()
             else:
-                left += 1
-                right -= 1
-        return True
+                s_list[index] = ""
+            index += 1
+
+        return ''.join(s_list) == ''.join(s_list[::-1])
