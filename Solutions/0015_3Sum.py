e0a70dbf8f7ab7927c55da7270c15ac9d853c39f
Won Yong Ha <hpmsora@gmail.com>
2024-11-30 01:37:37 -0500

    Update 0015_3Sum.py

--- Solutions/0015_3Sum.py
+++ Solutions/0015_3Sum.py
@@ -1,55 +1,58 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        p_dict = {}
+        p_freq = {}
         p_list = []
-        n_dict = {}
+        n_freq = {}
         n_list = []
-        z_dict = {}
+        z_freq = {}
         
         for each_nums in nums:
-            if each_nums == 0:
-                if each_nums in z_dict:
-                    z_dict[each_nums] += 1
+            if each_nums > 0:
+                if each_nums in p_freq:
+                    p_freq[each_nums] += 1
                 else:
-                    z_dict[each_nums] = 1
-            elif each_nums > 0:
-                if each_nums in p_dict:
-                    p_dict[each_nums] += 1
-                else:
-                    p_dict[each_nums] = 1
+                    p_freq[each_nums] = 1
                     p_list.append(each_nums)
-            else:
-                if each_nums in n_dict:
-                    n_dict[each_nums] += 1
+            elif each_nums == 0:
+                if each_nums in z_freq:
+                    z_freq[each_nums] += 1
                 else:
-                    n_dict[each_nums] = 1
+                    z_freq[each_nums] = 1
+            else:
+                if each_nums in n_freq:
+                    n_freq[each_nums] += 1
+                else:
+                    n_freq[each_nums] = 1
                     n_list.append(each_nums)
         
+        # solution variable
         sol = []
         
         # 1. 0 + 0 + 0
-        if z_dict and z_dict[0] >= 3:
+        if 0 in z_freq and z_freq[0] >= 3:
             sol.append([0, 0, 0])
-        # 2. p + n + 0
-        if z_dict and z_dict[0] >= 1:
-            for each_p_list in p_list:
-                if each_p_list * -1 in n_dict:
-                    sol.append([each_p_list, each_p_list*-1, 0])
-        # 3. p + p + n
-        for index, each_p_1 in enumerate(p_list):
-            # same two p
-            if p_dict[each_p_1] >= 2 and each_p_1*-2 in n_dict:
-                sol.append([each_p_1, each_p_1, each_p_1*-2])
-            for each_p_2 in p_list[index+1:]:
-                if (each_p_1 + each_p_2) * -1 in n_dict:
-                    sol.append([each_p_1, each_p_2, (each_p_1+each_p_2)*-1])
-        # 4. n + n + p
-        for index, each_n_1 in enumerate(n_list):
-            # same two n
-            if n_dict[each_n_1] >= 2 and each_n_1*-2 in p_dict:
-                sol.append([each_n_1, each_n_1, each_n_1*-2])
-            for each_n_2 in n_list[index+1:]:
-                if (each_n_1 + each_n_2) * -1 in p_dict:
-                    sol.append([each_n_1, each_n_2, (each_n_1+each_n_2)*-1])
-                        
+        # 2. 0 + P + N
+        if 0 in z_freq and z_freq[0] >= 1:
+            for each_p in p_freq.keys():
+                if each_p * -1 in n_freq:
+                    sol.append([0, each_p, -1 * each_p])
+        # 3. P1 + P2 + N
+        for index, each_p1 in enumerate(p_list):
+            # 3.1 P1 == P2
+            if p_freq[each_p1] >= 2 and each_p1 * -2 in n_freq:
+                sol.append([each_p1, each_p1, -2 * each_p1])
+            if index < len(p_list) - 1:
+                for each_p2 in p_list[index+1:]:
+                    if (each_p1 + each_p2) * -1 in n_freq:
+                        sol.append([each_p1, each_p2, (each_p1 + each_p2) * -1])
+        # 4. N1 + N2 + P
+        for index, each_n1 in enumerate(n_list):
+            # 3.1 N1 == N2
+            if n_freq[each_n1] >= 2 and each_n1 * -2 in p_freq:
+                sol.append([each_n1, each_n1, -2 * each_n1])
+            if index < len(n_list) - 1:
+                for each_n2 in n_list[index+1:]:
+                    if (each_n1 + each_n2) * -1 in p_freq:
+                        sol.append([each_n1, each_n2, (each_n1 + each_n2) * -1])
+        # RETURN
         return sol
