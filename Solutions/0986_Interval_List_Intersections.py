333f94c3c7d0a8e9c2341c19366c5d27766fb8a6
Won Yong Ha <hpmsora@gmail.com>
2024-12-01 23:39:31 -0500

    Update 0986_Interval_List_Intersections.py

--- Solutions/0986_Interval_List_Intersections.py
+++ Solutions/0986_Interval_List_Intersections.py
@@ -1,43 +1,41 @@
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
-        def common_interval(_interval_1, _interval_2):
-            start_1, end_1 = _interval_1
-            start_2, end_2 = _interval_2
-
-            start_sol = -1
-            if start_1 > start_2:
-                start_sol = start_1
-            else:
-                start_sol = start_2
-
-            end_sol = -2
-            if end_1 < end_2:
-                end_sol = end_1
-            else:
-                end_sol = end_2
-
-            isRemove = False
-            if end_1 >= end_2:
-                isRemove = True
-
-            if start_sol <= end_sol:
-                return ([start_sol, end_sol], isRemove)
-            else:
-                return ([], isRemove)
+        index_2 = 0
 
         sol = []
-        for each_firstList in firstList:
-            if not secondList:
-                break
-            while secondList:
-                sub_list = secondList[0]
-                temp_sol, isRemove = common_interval(each_firstList, sub_list)
 
-                if temp_sol:
-                    sol.append(temp_sol)
-                
-                if isRemove:
-                    secondList.pop(0)
+        for start, end in firstList:
+            while index_2 < len(secondList) and secondList[index_2][1] < start:
+                index_2 += 1
+            if not index_2 < len(secondList):
+                break
+            
+
+            # B Start < A Start
+            if secondList[index_2][0] < start:
+                # B Start < A Start < B End < A End
+                if secondList[index_2][1] >= start and secondList[index_2][1] <= end:
+                    sol.append([start, secondList[index_2][1]])
+                    index_2 += 1
+                # B Start < A Start < A End < B End
+                elif secondList[index_2][1] > end:
+                    sol.append([start, end])
+                    continue
+
+            # A Start < B Start
+            # second index stretch
+            while index_2 < len(secondList) and start > secondList[index_2][1]:
+                index_2 += 1
+            
+            # A Start < B Start
+            while index_2 < len(secondList) and secondList[index_2][0] <= end:
+                # A Start < B Start < B End < A End
+                if secondList[index_2][1] <= end:
+                    sol.append(secondList[index_2])
+                # A Start < B Start < A End < B End
                 else:
+                    sol.append([secondList[index_2][0], end])
                     break
+                index_2 += 1
+
         return sol
