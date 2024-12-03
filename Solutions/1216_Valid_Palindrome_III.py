eb35daa91aa84051c4d0f6ade08a736c5a45ddf2
Won Yong Ha <hpmsora@gmail.com>
2024-12-03 16:24:53 -0500

    Update 1216_Valid_Palindrome_III.py

--- Solutions/1216_Valid_Palindrome_III.py
+++ Solutions/1216_Valid_Palindrome_III.py
@@ -1,39 +1,44 @@
 class Solution:
     def isValidPalindrome(self, s: str, k: int) -> bool:
         left = 0
-        right = len(s) - 1
-        s_list = list(s)
+        right = len(s)-1
 
         while left < right:
-            if s_list[left] == s_list[right]:
+            if s[left] == s[right]:
                 left += 1
                 right -= 1
-            else:
-                stack = collections.deque([(left+1, right, 1), (left, right-1, 1)])
-                visited = set()
-                visited.add((left+1, right))
-                visited.add((left, right+1))
-                while stack:
-                    n_left, n_right, step = stack.popleft()
 
+            else:
+                stack = collections.deque()
+                stack.append((left + 1, right, 1))
+                stack.append((left, right - 1, 1))
+                visited = set()
+                visited.add((left + 1, right))
+                visited.add((left, right - 1))
+
+                while stack:
+                    n_left, n_right, step= stack.popleft()
                     isSol = True
+
                     while n_left < n_right:
-                        if n_right - n_left + step <= k:
-                            return True
-                        if s_list[n_left] == s_list[n_right]:
+                        if s[n_left] == s[n_right]:
                             n_left += 1
                             n_right -= 1
                         else:
-                            if step < k:
-                                if not (n_left+1, n_right) in visited:
-                                    stack.append((n_left+1, n_right, step + 1))
-                                    visited.add((n_left+1, n_right))
-                                if not (n_left, n_right-1) in visited:
-                                    stack.append((n_left, n_right-1, step + 1))
-                                    visited.add((n_left, n_right-1))
                             isSol = False
-                            break
+                            if step < k:
+                                if n_left + 1 <= n_right and not (n_left + 1, n_right) in visited:
+                                    visited.add((n_left + 1, n_right))
+                                    stack.append((n_left + 1, n_right, step+1))
+                                if n_left <= n_right - 1 and not (n_left, n_right - 1) in visited:
+                                    visited.add((n_left, n_right - 1))
+                                    stack.append((n_left, n_right - 1, step+1))
+                                break
+                            else:
+                                break
                     if isSol:
                         return True
                 return False
         return True
+
+                            
