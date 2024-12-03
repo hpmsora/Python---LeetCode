ffa450b188ce21eb53cced4c083d64b90a63e3db
Won Yong Ha <hpmsora@gmail.com>
2024-12-02 23:57:05 -0500

    Update 0463_Island_Perimeter.py

--- Solutions/0463_Island_Perimeter.py
+++ Solutions/0463_Island_Perimeter.py
@@ -1,60 +1,32 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        max_x = len(grid)
-        max_y = len(grid[0])
-        curr_cell_x, curr_cell_y = (0, 0)
-
-        for x in range(max_x):
-            isFound = False
-            for y in range(max_y):
+        m = len(grid)
+        n = len(grid[0])
+        
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        
+        start = (0, 0)
+        # Find land first
+        for x in range(m):
+            for y in range(n):
                 if grid[x][y] == 1:
-                    curr_cell_x, curr_cell_y = (x, y)
-                    isFound = True
-                    break
-            if isFound:
-                break
-
-        seen = set()
-        seen.add((curr_cell_x, curr_cell_y))
-
-        def helper(_x, _y):
-            print(_x, _y)
-            sol = 0
-            # x + 1, y
-            if _x+1 < max_x:
-                if grid[_x + 1][_y]==0:
+                    start = (x, y)
+        
+        # Find island corners
+        stack = collections.deque([start])
+        sol = 0
+        visited = set()
+        visited.add(start)
+        while stack:
+            x, y = stack.popleft()
+            
+            for dx, dy in dirs:
+                nx, ny = x + dx, y + dy
+                
+                if nx >= 0 and nx < m and ny >= 0 and ny < n and grid[nx][ny] == 1:
+                    if not (nx, ny) in visited:
+                        visited.add((nx, ny))
+                        stack.append((nx, ny))
+                else:
                     sol += 1
-                elif not (_x+1, _y) in seen:
-                    seen.add((_x+1, _y))
-                    sol += helper(_x+1, _y)
-            else:
-                sol += 1
-            # x - 1, y
-            if _x-1 >= 0:
-                if grid[_x - 1][_y]==0:
-                    sol += 1
-                elif not (_x-1, _y) in seen:
-                    seen.add((_x-1, _y))
-                    sol += helper(_x-1, _y)
-            else:
-                sol += 1
-            # x, y + 1
-            if _y+1 < max_y:
-                if grid[_x][_y+1]==0:
-                    sol += 1
-                elif not (_x, _y+1) in seen:
-                    seen.add((_x, _y+1))
-                    sol += helper(_x, _y+1)
-            else:
-                sol += 1
-            # x, y - 1
-            if _y-1 >= 0:
-                if grid[_x][_y-1]==0:
-                    sol += 1
-                elif not (_x, _y-1) in seen:
-                    seen.add((_x, _y-1))
-                    sol += helper(_x, _y-1)
-            else:
-                sol += 1
-            return sol
-        return helper(curr_cell_x, curr_cell_y)
+        return sol
