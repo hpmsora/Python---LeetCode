3557555ef5f78883769aadf12ac9d6b4e3203e30
Won Yong Ha <hpmsora@gmail.com>
2024-12-03 18:49:31 -0500

    Update 0088_Merge_Sorted_Array.py

--- Solutions/0088_Merge_Sorted_Array.py
+++ Solutions/0088_Merge_Sorted_Array.py
@@ -3,22 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-
         index_1 = m - 1
         index_2 = n - 1
+        index_m = len(nums1) - 1
 
         while index_1 >= 0 or index_2 >= 0:
-
-            if index_1 < 0:
-                nums1[index_2] = nums2[index_2]
+            if not index_1 >= 0:
+                nums1[index_m] = nums2[index_2]
                 index_2 -= 1
-            elif index_2 < 0:
-                nums1[index_1] = nums1[index_1]
+            elif not index_2 >= 0:
+                nums1[index_m] = nums1[index_1]
                 index_1 -= 1
-
             elif nums1[index_1] > nums2[index_2]:
-                nums1[index_1 + index_2+1] = nums1[index_1]
+                nums1[index_m] = nums1[index_1]
                 index_1 -= 1
             else:
-                nums1[index_1 + index_2+1] = nums2[index_2]
+                nums1[index_m] = nums2[index_2]
                 index_2 -= 1
+            index_m -= 1
