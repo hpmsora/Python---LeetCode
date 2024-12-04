9151eae8aa890e048b4bec853c4710f849e81e97
Won Yong Ha <hpmsora@gmail.com>
2024-12-04 11:23:47 -0500

    Update 1249_Minimum_Remove_to_Make_Valid_Parentheses.py

--- Solutions/1249_Minimum_Remove_to_Make_Valid_Parentheses.py
+++ Solutions/1249_Minimum_Remove_to_Make_Valid_Parentheses.py
@@ -1,27 +1,18 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        # [(str("(" or ")"), int(index))]
-        stack = []
-
-        # str(s) -> list(s)
-        s_list = list(s)
-
-        for index, each_s in enumerate(s_list):
-            # Case 1: "("
-            if each_s == "(":
-                stack.append(("(", index))
-            # Case 2: ")"
-            elif each_s == ")":
-                # Case 2-1: remove "("
-                if stack and stack[-1][0] == "(":
-                    stack.pop()
-                # Case 2-2: append ")"
-                else:
-                    stack.append((")", index))
         
-        # str(s) remove unnecessary parenthesis
-        for _, index in stack:
+        s_list = list(s)
+        par_list = []
+        for index, each_s in enumerate(s_list):
+            if each_s == "(":
+                par_list.append(("(", index))
+            elif each_s  == ")":
+                if par_list and par_list[-1][0] == "(":
+                    par_list.pop()
+                else:
+                    par_list.append((")", index))
+        
+        for _, index in par_list:
             s_list[index] = ""
         
-        # RETURN - list(s_list) -> str(s)
         return ''.join(s_list)
