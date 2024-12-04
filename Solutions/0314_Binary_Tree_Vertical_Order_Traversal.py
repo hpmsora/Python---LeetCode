326d8c646dfb56b4b0beb7ce7b7c72c3f1bfa579
Won Yong Ha <hpmsora@gmail.com>
2024-12-04 12:27:29 -0500

    Update 0314_Binary_Tree_Vertical_Order_Traversal.py

--- Solutions/0314_Binary_Tree_Vertical_Order_Traversal.py
+++ Solutions/0314_Binary_Tree_Vertical_Order_Traversal.py
@@ -8,30 +8,29 @@ class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
-        left = 0
-        right = 0
-        data_dict = {}
-        
-        queue = collections.deque([(root, 0)])
-        
-        while queue:
-            curr_node, level = queue.popleft()
-            left = min(left, level)
-            right = max(right, level)
-            
-            if level in data_dict:
-                data_dict[level].append(curr_node.val)
-            else:
-                data_dict[level] = [curr_node.val]
-            
+        # BFS
+        stack = [(root, 0)]
+
+        index = 0
+        min_order = 0
+        max_order = 0
+
+        while index < len(stack):
+            curr_node, order = stack[index]
+            min_order = min(min_order, order)
+            max_order = max(max_order, order)
+
             if curr_node.left:
-                queue.append((curr_node.left, level-1))
+                stack.append((curr_node.left, order-1))
             
             if curr_node.right:
-                queue.append((curr_node.right, level+1))
-                
-        sol = [[] for _ in range(right - left + 1)]
+                stack.append((curr_node.right, order + 1))
+            index += 1
         
-        for index, data in data_dict.items():
-            sol[index-left] = data
+        sol = [[] for _ in range((max_order - min_order+1))]
+
+        for node, order in stack:
+            order = order - min_order
+            sol[order].append(node.val)
+        
         return sol
