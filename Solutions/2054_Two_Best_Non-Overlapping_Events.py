b4ccf9cf1d45ea6d22316f98c8bbea2a0d0dc655
Won Yong Ha <hpmsora@gmail.com>
2024-12-09 14:21:31 -0500

    2054. Two Best Non-Overlapping Events
    2054. Two Best Non-Overlapping Events helped

---
+++ Solutions/2054_Two_Best_Non-Overlapping_Events.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxTwoEvents(self, events):
+        events.sort()
+        pq = []
+        max_val = 0
+        ans = 0
+
+        for start, end, value in events:
+            while pq and pq[0][0] < start:
+                max_val = max(max_val, heapq.heappop(pq)[1])
+            ans = max(ans, max_val + value)
+            heapq.heappush(pq, (end, value))
+
+        return ans
