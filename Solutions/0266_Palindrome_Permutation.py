1e99e65623e11b854edd1714672821e116dc9919
Won Yong Ha <hpmsora@gmail.com>
2024-12-27 13:36:16 -0500

    Update 0266_Palindrome_Permutation.py

--- Solutions/0266_Palindrome_Permutation.py
+++ Solutions/0266_Palindrome_Permutation.py
@@ -1,16 +1,17 @@
 class Solution:
     def canPermutePalindrome(self, s: str) -> bool:
-        letter_count = {}
+        letter_freq_dict = {}
 
         for each_s in s:
-            if each_s in letter_count:
-                letter_count[each_s] += 1
+            if each_s in letter_freq_dict:
+                letter_freq_dict[each_s] += 1
             else:
-                letter_count[each_s] = 1
-        
+                letter_freq_dict[each_s] = 1
+
         odd_count = 0
-        for value in letter_count.values():
-            if value % 2 == 1:
+
+        for freq in letter_freq_dict.values():
+            if freq % 2 == 1:
                 odd_count += 1
                 if odd_count > 1:
                     return False
