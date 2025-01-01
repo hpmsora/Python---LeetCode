c135fc34051dc4f2fd6f6fca6cfa07e1d9d5c740
Won Yong Ha <hpmsora@gmail.com>
2025-01-01 10:27:58 -0500

    Update 0921_Minimum_Add_to_Make_Parentheses_Valid.py

--- Solutions/0921_Minimum_Add_to_Make_Parentheses_Valid.py
+++ Solutions/0921_Minimum_Add_to_Make_Parentheses_Valid.py
@@ -1,30 +1,13 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        # Declare balance count variable
-        b_index = 0
-        
-        # Declare number of addition
-        addition = 0
+        stack = collections.deque()
 
-        # Loop - each letter of string s
         for each_s in s:
-            
-            # Check letter - open par
             if each_s == "(":
-                b_index += 1
-            # Check letter - close par
+                stack.append("(")
             elif each_s == ")":
-                b_index -= 1
-
-                # Check close par not feasible
-                if b_index < 0:
-                    addition += 1
-                    b_index += 1
-
-        # RETURN - conditional
-        # 1. Unblanced
-        if b_index > 0:
-            return b_index + addition
-        # 2. Balanced
-        else:
-            return addition
+                if stack and stack[-1] == "(":
+                    stack.pop()
+                else:
+                    stack.append(")")
+        return len(stack)
