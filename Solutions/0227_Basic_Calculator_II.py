31865036d280876db25ed8d28093135baa2c4db4
Won Yong Ha <hpmsora@gmail.com>
2025-01-02 10:49:39 -0500

    Update 0227_Basic_Calculator_II.py

--- Solutions/0227_Basic_Calculator_II.py
+++ Solutions/0227_Basic_Calculator_II.py
@@ -1,46 +1,46 @@
 class Solution:
     def calculate(self, s: str) -> int:
+        m_d = set(["*", "/"])
+        p_m = set(["+", "-"])
+
         stack = []
-        m_d_set = set(["*", "/"])
-        p_n_set = set(["+", "-"])
 
-        num = ""
-
+        nums_str = ""
         for each_s in s:
             if each_s == " ":
                 continue
-            if each_s in m_d_set or each_s in p_n_set:
-                curr_num = int(num)
-                num = ""
-                if stack and stack[-1] in m_d_set:
-                    sign = stack.pop()
-                    if sign == "*":
-                        stack.append(stack.pop() * curr_num)
+            if each_s in m_d or each_s in p_m:
+                nums = int(nums_str)
+                if stack and stack[-1] in m_d:
+                    if stack[-1] == "*":
+                        stack.pop()
+                        stack[-1] = stack[-1] * nums
                     else:
-                        stack.append(int(stack.pop() / curr_num))
+                        stack.pop()
+                        stack[-1] = int(stack[-1] / nums)
                 else:
-                    stack.append(curr_num)
-
+                    stack.append(nums)
                 stack.append(each_s)
+                nums_str = ""
             else:
-                num += each_s
-        if not stack:
-            stack.append(int(num))
-        elif stack[-1] in m_d_set:
-            sign = stack.pop()
-            if sign == "*":
-                stack.append(stack.pop() * int(num))
+                nums_str += each_s
+        if stack:
+            if stack[-1] == "*":
+                stack.pop()
+                stack[-1] = stack[-1] * int(nums_str)
+            elif stack[-1] == "/":
+                stack.pop()
+                stack[-1] = int(stack[-1] / int(nums_str))
             else:
-                stack.append(int(stack.pop() / int(num)))
+                stack.append(int(nums_str))
         else:
-            stack.append(int(num))
-
-        index = 1
+            stack.append(int(nums_str))
         sol = stack[0]
+        index = 1
         while index < len(stack):
-            curr = stack[index]
+            sign = stack[index]
             index += 1
-            if curr == "+":
+            if sign == "+":
                 sol += stack[index]
             else:
                 sol -= stack[index]
