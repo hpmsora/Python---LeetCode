5eb73e2d48ef37365d497b3c3db53996eb5eaf5a
Won Yong Ha <hpmsora@gmail.com>
2025-01-02 16:00:28 -0500

    Update 1091_Shortest_Path_in_Binary_Matrix.py

--- Solutions/1091_Shortest_Path_in_Binary_Matrix.py
+++ Solutions/1091_Shortest_Path_in_Binary_Matrix.py
@@ -1,31 +1,29 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
+        dirs = [(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]
 
-        target = (m-1, n-1)
-
-        dirs = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
-
-        if grid[0][0] == 1 or grid[m-1][n-1] == 1:
+        n = len(grid)
+        start = (0, 0)
+        end = (n-1, n-1)
+        if grid[0][0] == 1:
             return -1
-        elif m-1 == 0 and n-1 == 0:
+        elif start == end:
             return 1
 
-        stack = collections.deque([(0, 0, 1)])
-        visited = set([(0, 0)])
-
+        stack = collections.deque([(start, 1)])
+        visited = set()
+        visited.add(start)
         while stack:
-            x, y, step = stack.popleft()
-
+            curr_pos, step = stack.popleft()
+            
+            x, y = curr_pos
             for dx, dy in dirs:
                 nx, ny = x + dx, y + dy
-
-                if nx >= 0 and nx < m and ny >= 0 and ny < n and not (nx, ny) in visited:
+                if nx >= 0 and nx < n and ny >= 0 and ny < n and not (nx, ny) in visited:
                     if grid[nx][ny] == 1:
                         continue
-                    if (nx, ny) == target:
+                    if (nx, ny) == end:
                         return step + 1
                     visited.add((nx, ny))
-                    stack.append((nx, ny, step + 1))
+                    stack.append(((nx, ny), step + 1))
         return -1
