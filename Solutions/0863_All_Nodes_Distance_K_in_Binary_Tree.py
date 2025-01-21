e727e1d760d4208845237ea086fef94c08335f8f
Won Yong Ha <hpmsora@gmail.com>
2025-01-20 22:20:02 -0500

    Update 0863_All_Nodes_Distance_K_in_Binary_Tree.py

--- Solutions/0863_All_Nodes_Distance_K_in_Binary_Tree.py
+++ Solutions/0863_All_Nodes_Distance_K_in_Binary_Tree.py
@@ -7,97 +7,42 @@
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        # Return varialbe
+        parent_node_list = []
+
+        def dfs(_root, _parent_list):
+            nonlocal parent_node_list
+            if _root == target:
+                parent_node_list = _parent_list
+                return
+            if _root.left:
+                dfs(_root.left, [_root] + _parent_list)
+            
+            if _root.right:
+                dfs(_root.right, [_root] + _parent_list)
+        dfs(root, [])
+
         sol = []
 
-        # Helper function - BFS (find val under _root with k distance)
-        def helper(_root, _k):
-            if _k == 0:
-                return [_root.val]
-
-            # Leaf check
-            if not (_root.left or _root.right):
-                return []
-            
-            # Left check
-            left = []
-            if _root.left:
-                left = helper(_root.left, _k - 1)
-
-            # Right check
-            right = []
-            if _root.right:
-                right = helper(_root.right, _k - 1)
-
-            # RETURN
-            return left + right
-
-        # Helper function - BFS (find ancestor)
-        def helper_2(_root):
-            nonlocal target
-            if target == _root:
-                return [_root]
-
-            # Leaf check
-            if not (_root.left or _root.right):
-                return []
-            
-            # Left check
-            if _root.left:
-                left = helper_2(_root.left)
-                if left:
-                    return left + [_root]
-
-            # Right check
-            if _root.right:
-                right = helper_2(_root.right)
-                if right:
-                    return right + [_root]
-
-
-            # RETURN
-            return []
-
-        # Helper function - BFS (find val under _root with k distance except visited)
-        def helper_3(_root, _k, _exception):
-            # Exception break
-            if _root == _exception:
-                return []
-
-            if _k == 0:
-                return [_root.val]
-
-            # Leaf check
-            if not (_root.left or _root.right):
-                return []
-            
-            # Left check
-            left = []
-            if _root.left:
-                left = helper_3(_root.left, _k - 1, _exception)
-
-            # Right check
-            right = []
-            if _root.right:
-                right = helper_3(_root.right, _k - 1, _exception)
-
-            # RETURN
-            return left + right
-
-        # 1. Check child of target
-        sol = sol + helper(target, k)
-
-        # 2. Find ancesstor of target
-        ancestor = helper_2(root)
+        # 1. below currnet target
+        def dfs_k(_root, _steps, _exception):
+            nonlocal sol
+            if _steps == 0:
+                sol.append(_root.val)
+                return
+            if _root.left and not _root.left == _exception:
+                dfs_k(_root.left, _steps - 1, _exception)
+            if _root.right and not _root.right == _exception:
+                dfs_k(_root.right, _steps - 1, _exception)
         
-        # 3. Find ancestor's target distance
-        # Loop all acestor iteration except target
-        prev = ancestor[0] # BFS exception (previous ancestor)
-        for index, each_ancestor in enumerate(ancestor[1:]):
-            sol = sol + helper_3(each_ancestor, k - index - 1, prev)
+        dfs_k(target, k, None)
 
-            # Update previous ancestor
-            prev = each_ancestor
-        
-        # RETURN
+        # 2. Above target
+        prev_node = target
+        k_steps = k - 1
+        for each_parent in parent_node_list:
+            dfs_k(each_parent, k_steps, prev_node)
+            prev_node = each_parent
+            k_steps -= 1
+            if k_steps < 0:
+                break
         return sol
