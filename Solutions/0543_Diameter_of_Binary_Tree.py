58c5939ac078b9f2181793adcfd7e037f9beee1c
Won Yong Ha <hpmsora@gmail.com>
2025-01-26 16:34:12 -0500

    Update 0543_Diameter_of_Binary_Tree.py

--- Solutions/0543_Diameter_of_Binary_Tree.py
+++ Solutions/0543_Diameter_of_Binary_Tree.py
@@ -7,20 +7,22 @@
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         sol = 0
-        # dfs
         def dfs(_root):
             nonlocal sol
+
             if not (_root.left or _root.right):
-                return 0
+                return 1
             left = 0
             if _root.left:
-                left = dfs(_root.left) + 1
-
+                left = dfs(_root.left)
             right = 0
             if _root.right:
-                right = dfs(_root.right) + 1
+                right = dfs(_root.right)
+            
+            sol = max(sol, left + right)
 
-            sol = max(sol, left+right)
+            return max(left, right) + 1
+        
+        dfs(root)
 
-            return max(left, right)
-        return max(dfs(root), sol)
+        return sol
