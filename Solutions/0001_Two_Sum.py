bcbb06d57ad7a2856573384cfa1343c64f38a7c0
Won Yong Ha <hpmsora@gmail.com>
2025-06-04 16:50:50 -0400

    Update 0001_Two_Sum.py

--- Solutions/0001_Two_Sum.py
+++ Solutions/0001_Two_Sum.py
@@ -1,8 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_dict = {}
-
+        num_index_dict = {}
         for index, each_nums in enumerate(nums):
-            if target - each_nums in nums_dict:
-                return [index, nums_dict[target-each_nums]]
-            nums_dict[each_nums] = index
+            if target-each_nums in num_index_dict:
+                return [num_index_dict[target-each_nums], index]
+            num_index_dict[each_nums] = index
