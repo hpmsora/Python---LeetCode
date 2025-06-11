a7dacef81425855d46a2d696dc5112032ca35772
Won Yong Ha <hpmsora@gmail.com>
2025-06-11 15:55:11 -0400

    Update 0138_Copy_List_with_Random_Pointer.py

--- Solutions/0138_Copy_List_with_Random_Pointer.py
+++ Solutions/0138_Copy_List_with_Random_Pointer.py
@@ -9,19 +9,24 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        if not head:
-            return None
         old_to_new = {}
+
+        queue = head
+        if not head:
+            return
+
+        while queue:
+            old_to_new[queue] = Node(x=queue.val)
+            queue = queue.next
         
-        curr = head
-        while curr:
-            old_to_new[curr] = Node(curr.val)
-            curr = curr.next
-        
-        curr = head
-        while curr:
-            old_to_new[curr].next = old_to_new.get(curr.next)
-            old_to_new[curr].random = old_to_new.get(curr.random)
-            curr = curr.next
+        sol = old_to_new[head]
+        queue = head
+        while queue:
+            new_node = old_to_new[queue]
+            if queue.random:
+                new_node.random = old_to_new[queue.random]
             
-        return old_to_new[head]
+            if queue.next:
+                new_node.next = old_to_new[queue.next]
+            queue = queue.next
+        return sol
